31123a5f3a52547ce49c0d17fb24b93398668cbb
ParkSuah <48156241+ParkSuah@users.noreply.github.com>
2020-06-16 13:58:32 +0900

    Update process1.c

--- process1.c
+++ process1.c
@@ -5,15 +5,15 @@
 #include <sys/wait.h>
 
 int main(){
-	pid_t child;
+	pid_t child; // child process id
 	printf("program start\n");
 	child = fork();
-	if(child == 0){
+	if(child == 0){ // child process
 		sleep(2);
 		printf("I am child!\n");
 		//exit(0);
 	}
-	else if(child > 0){
+	else if(child > 0){ // parent process  Q: then what is the main process?
 		wait(0x0);
 		printf("I am parent!\n");
 		//wait(0x0);
